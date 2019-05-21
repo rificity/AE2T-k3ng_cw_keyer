96aabe666a8f0dd84802069a43b077fe9cbdbb86
Al Gritzmacher <al@warlocks1507.com>
2019-05-21 11:48:11 -0400

    Update keyer_hardware.h

--- k3ng_keyer/keyer_hardware.h
+++ k3ng_keyer/keyer_hardware.h
@@ -9,41 +9,11 @@
 
 */
 
- #define HARDWARE_NANOKEYER_REV_D   // https://nanokeyer.wordpress.com/nanokeyer-info/  edit these files: keyer_pin_settings_nanokeyer_rev_d.h, keyer_features_and_options_nanokeyer_rev_d.h, keyer_settings_nanokeyer_rev_d.h  
+// #define HARDWARE_NANOKEYER_REV_D   // https://nanokeyer.wordpress.com/nanokeyer-info/  edit these files: keyer_pin_settings_nanokeyer_rev_d.h, keyer_features_and_options_nanokeyer_rev_d.h, keyer_settings_nanokeyer_rev_d.h  
 // #define HARDWARE_K5BCQ   // edit these files: keyer_pin_settings_k5bcq.h, keyer_features_and_options_k5bcq.h, keyer_settings_k5bcq.h 
-// #define HARDWARE_MEGAKEYER // https://github.com/w6ipa/megakeyer
+ #define HARDWARE_MEGAKEYER // https://github.com/w6ipa/megakeyer
 // #define HARDWARE_TEST_EVERYTHING
 // #define HARDWARE_TEST
-
-
-/* 
-
-
-    HARDWARE_GENERIC_STM32F103C  (Contributed by sp5iou)
-
-      How to deal with those boards with Arduino: https://www.techshopbd.com/uploads/product_document/STM32bluepillarduinoguide(1).pdf
-
-
-    Funtronics FK-10 Programming Notes (Contributed by Disneysw 2016-12-10)
-
-      Programming the unit is accomplished by selecting "Mega2560" as the target processor and uploading to the rear USB port with the front
-      switch set to the Arduino position
-
-      Note: in order to get the FK-10 USB Host port working correctly you will need to patch the file "UsbCore.h" in the USB_Host_Shield library.
-      At the time of writing it is line 41 that needs modified to change "P10" to "P53" i.e. from
-
-           #else
-             typedef MAX3421e<P10, P9> MAX3421E; // Official Arduinos (UNO, Duemilanove, Mega, 2560, Leonardo, Due etc.) or Teensy 2.0 and 3.0
-           #endif
-
-      to:
-
-           #else
-             typedef MAX3421e<P53, P9> MAX3421E; // Official Arduinos (UNO, Duemilanove, Mega, 2560, Leonardo, Due etc.) or Teensy 2.0 and 3.0
-           #endif
-
-
-*/
 
 
 // Do not touch anything below this line!
