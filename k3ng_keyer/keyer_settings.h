a8d284d432fe29ac16631c95a4f9f8d38cd53dc5
Al Gritzmacher <al@warlocks1507.com>
2019-05-21 11:09:33 -0400

    Update keyer_settings.h
    custom say hi

--- k3ng_keyer/keyer_settings.h
+++ k3ng_keyer/keyer_settings.h
@@ -1,4 +1,5 @@
 // Initial and hardcoded settings
+#define custom_say_hi_message "HI"
 #define initial_speed_wpm 26             // "factory default" keyer speed setting
 #define initial_command_mode_speed_wpm 20 // "factory default" command mode speed setting 
 #define initial_sidetone_freq 600        // "factory default" sidetone frequency setting
