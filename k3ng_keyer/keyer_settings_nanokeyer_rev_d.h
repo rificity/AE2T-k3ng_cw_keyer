0fb5d1af4e26a8a01b9ec12e9dc91ba1fb5d2494
Al Gritzmacher <albert@gritzmacher.net>
2019-05-21 14:39:47 -0400

    Update to local copy

--- k3ng_keyer/keyer_settings_nanokeyer_rev_d.h
+++ k3ng_keyer/keyer_settings_nanokeyer_rev_d.h
@@ -1,5 +1,4 @@
 // Initial and hardcoded settings
-#define custom_say_hi_message "HI DE AE2T"
 #define initial_speed_wpm 26             // "factory default" keyer speed setting
 #define initial_command_mode_speed_wpm 20 // "factory default" command mode speed setting 
 #define initial_sidetone_freq 600        // "factory default" sidetone frequency setting
@@ -21,9 +20,9 @@
 #define initial_ptt_lead_time_tx6 0         // PTT lead time in mS
 #define initial_ptt_tail_time_tx6 10         // PTT tail time in mS
 #define initial_qrss_dit_length 1        // QRSS dit length in seconds
-#define initial_pot_wpm_low_value 13     // Potentiometer WPM fully CCW
-#define initial_pot_wpm_high_value 35    // Potentiometer WPM fully CW
-#define wpm_limit_low 5
+#define initial_pot_wpm_low_value 10     // Potentiometer WPM fully CCW
+#define initial_pot_wpm_high_value 60    // Potentiometer WPM fully CW
+#define wpm_limit_low 10
 #define wpm_limit_high 60
 #define potentiometer_change_threshold 0.9 // don't change the keyer speed until pot wpm has changed more than this
 #define send_buffer_size 150
@@ -43,6 +42,7 @@
 #define default_memory_repeat_time 3000  // time in milliseconds
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
+#define lcd_i2c_address_mathertel_PCF8574 0x27             // I2C address of display
 #define hell_pixel_microseconds 4025
 #define program_memory_limit_consec_spaces 1
 #define serial_leading_zeros 1            // set to 1 to activate leading zeros in serial numbers (i.e. #1 = 001)
@@ -68,33 +68,33 @@
 #define eeprom_write_time_ms 30000
 
 #ifdef FEATURE_COMMAND_BUTTONS
-#define analog_buttons_number_of_buttons 4
-#define analog_buttons_r1 10
-#define analog_buttons_r2 1
+  #define analog_buttons_number_of_buttons 4  // includes the command button (command button + 3 memory buttons = 4)
+  #define analog_buttons_r1 10
+  #define analog_buttons_r2 1
 #endif
 
 
 #if defined(FEATURE_COMMAND_BUTTONS) &&  !defined(FEATURE_PS2_KEYBOARD) && !defined(FEATURE_USB_KEYBOARD) && !defined(FEATURE_COMMAND_LINE_INTERFACE) && !defined(FEATURE_WINKEY_EMULATION)
-#define number_of_memories byte(analog_buttons_number_of_buttons-1)
+  #define number_of_memories byte(analog_buttons_number_of_buttons-1)
 #else
-#define number_of_memories byte(12)
+  #define number_of_memories byte(12)
 #endif
 
 #ifdef FEATURE_CAPACITIVE_PADDLE_PINS
-#define capacitance_threshold 2
+  #define capacitance_threshold 2
 #endif //FEATURE_CAPACITIVE_PADDLE_PINS
 
 #ifdef FEATURE_LED_RING
-#define led_ring_low_limit 10
-#define led_ring_high_limit 50
+  #define led_ring_low_limit 10
+  #define led_ring_high_limit 50
 #endif //FEATURE_LED_RING
 
 #ifdef FEATURE_QLF
-#define qlf_dit_max 125
-#define qlf_dit_min 75
-#define qlf_dah_max 200
-#define qlf_dah_min 100
-#define qlf_on_by_default 0
+  #define qlf_dit_max 125
+  #define qlf_dit_min 75
+  #define qlf_dah_max 200
+  #define qlf_dah_min 100
+  #define qlf_on_by_default 0
 #endif //FEATURE_QLF
 
 
@@ -105,28 +105,28 @@
     #define WINKEY_DEFAULT_BAUD 1200
   #endif //OPTION_WINKEY_UCXLOG_9600_BAUD
 // alter these below to map alternate sidetones for Winkey interface protocol emulation
-  #ifdef OPTION_WINKEY_2_SUPPORT
-    #define WINKEY_SIDETONE_1 3759
-    #define WINKEY_SIDETONE_2 1879
-    #define WINKEY_SIDETONE_3 1252
-    #define WINKEY_SIDETONE_4 940
-    #define WINKEY_SIDETONE_5 752
-    #define WINKEY_SIDETONE_6 625
-    #define WINKEY_SIDETONE_7 535
-    #define WINKEY_SIDETONE_8 469
-    #define WINKEY_SIDETONE_9 417
-    #define WINKEY_SIDETONE_10 375
-  #else //OPTION_WINKEY_2_SUPPORT
-    #define WINKEY_SIDETONE_1 4000
-    #define WINKEY_SIDETONE_2 2000
-    #define WINKEY_SIDETONE_3 1333
-    #define WINKEY_SIDETONE_4 1000
-    #define WINKEY_SIDETONE_5 800
-    #define WINKEY_SIDETONE_6 666
-    #define WINKEY_SIDETONE_7 571
-    #define WINKEY_SIDETONE_8 500
-    #define WINKEY_SIDETONE_9 444
-    #define WINKEY_SIDETONE_10 400
+#ifdef OPTION_WINKEY_2_SUPPORT
+	#define WINKEY_SIDETONE_1 3759
+	#define WINKEY_SIDETONE_2 1879
+	#define WINKEY_SIDETONE_3 1252
+	#define WINKEY_SIDETONE_4 940
+	#define WINKEY_SIDETONE_5 752
+	#define WINKEY_SIDETONE_6 625
+	#define WINKEY_SIDETONE_7 535
+	#define WINKEY_SIDETONE_8 469
+	#define WINKEY_SIDETONE_9 417
+	#define WINKEY_SIDETONE_10 375
+#else //OPTION_WINKEY_2_SUPPORT
+	#define WINKEY_SIDETONE_1 4000
+	#define WINKEY_SIDETONE_2 2000
+	#define WINKEY_SIDETONE_3 1333
+	#define WINKEY_SIDETONE_4 1000
+	#define WINKEY_SIDETONE_5 800
+	#define WINKEY_SIDETONE_6 666
+	#define WINKEY_SIDETONE_7 571
+	#define WINKEY_SIDETONE_8 500
+	#define WINKEY_SIDETONE_9 444
+	#define WINKEY_SIDETONE_10 400
 #endif //OPTION_WINKEY_2_SUPPORT
 
 #define WINKEY_1_REPORT_VERSION_NUMBER 10
@@ -140,6 +140,8 @@
 
 #endif //FEATURE_WINKEY_EMULATION
 
+
+
 #define PRIMARY_SERIAL_PORT &Serial
 #define PRIMARY_SERIAL_PORT_BAUD 115200     // This applies only when the port is in Command Line Interface mode.  In Winkey mode it will default to 1200.
 
@@ -148,10 +150,13 @@
   #define SECONDARY_SERIAL_PORT_BAUD 115200
 #endif
 
-#define CW_DECODER_SCREEN_COLUMNS 40        // word wrap at this many columns
+
+#define CW_DECODER_SCREEN_COLUMNS 120        // word wrap at this many columns
 #define CW_DECODER_SPACE_PRINT_THRESH 4.5   // print space if no tone for this many element lengths
 #define CW_DECODER_SPACE_DECODE_THRESH 2.0  // invoke character decode if no tone for this many element lengths
 #define CW_DECODER_NOISE_FILTER 20          // ignore elements shorter than this (mS)
+
+#define STRAIGHT_KEY_ACTIVE_STATE LOW
 
 #ifdef FEATURE_DYNAMIC_DAH_TO_DIT_RATIO
   #define DYNAMIC_DAH_TO_DIT_RATIO_LOWER_LIMIT_WPM 30
@@ -160,15 +165,22 @@
   #define DYNAMIC_DAH_TO_DIT_RATIO_UPPER_LIMIT_RATIO 240 // 240 = 2.4:1 ratio
 #endif //FEATURE_DYNAMIC_DAH_TO_DIT_RATIO
 
+#if defined(FEATURE_COMPETITION_COMPRESSION_DETECTION)
+  #define COMPETITION_COMPRESSION_DETECTION_ARRAY_SIZE 16
+  #define COMPETITION_COMPRESSION_DETECTION_TIME_INTERCHAR_LOWER_LIMIT 1
+  #define COMPETITION_COMPRESSION_DETECTION_TIME_INTERCHAR_UPPER_LIMIT 6.0
+  #define COMPETITION_COMPRESSION_DETECTION_AVERAGE_ALARM_THRESHOLD 3.0
+#endif //FEATURE_COMPETITION_COMPRESSION_DETECTION
+
 #if defined(FEATURE_SLEEP)
   #define KEYER_AWAKE_PIN_AWAKE_STATE HIGH
   #define KEYER_AWAKE_PIN_ASLEEP_STATE LOW
-#endif
+#endif 
 
 #if defined(FEATURE_ETHERNET)
   // #define FEATURE_ETHERNET_IP {192,168,1,178}                      // default IP address ("192.168.1.178")
   // #define FEATURE_ETHERNET_MAC {0xDE,0xAD,0xBE,0xEF,0xFE,0xED}
-  #define FEATURE_ETHERNET_IP {192,168,1,179}                      // default IP address ("192.168.1.178")
+  #define FEATURE_ETHERNET_IP {192,168,1,179}                      // default IP address ("192.168.1.179")
   #define FEATURE_ETHERNET_MAC {0xDE,0xAD,0xBE,0xEF,0xFE,0xEE}
 
   #define FEATURE_ETHERNET_GATEWAY {192,168,1,1}                   // default gateway
@@ -182,7 +194,7 @@
 
 #define FEATURE_INTERNET_LINK_MAX_LINKS 2
 #define FEATURE_INTERNET_LINK_DEFAULT_RCV_UDP_PORT 8888
-#define FEATURE_INTERNET_LINK_BUFFER_TIME_MS 500
+#define FEATURE_INTERNET_LINK_BUFFER_TIME_MS 500 
 
 #if defined(FEATURE_4x4_KEYPAD)|| defined (FEATURE_3x4_KEYPAD)
   #define KEYPAD_ROWS 4 //KeyPad Rows
@@ -237,9 +249,6 @@
 
 #define sequencer_pins_active_state HIGH
 #define sequencer_pins_inactive_state LOW
-
-#define sequencer_pins_active_state HIGH
-#define sequencer_pins_inactive_state LOW
 #define ptt_line_active_state HIGH
 #define ptt_line_inactive_state LOW
 #define tx_key_line_active_state HIGH
@@ -261,4 +270,3 @@
 
 #define sidetone_volume_low_limit 10
 #define sidetone_volume_high_limit 500
-
