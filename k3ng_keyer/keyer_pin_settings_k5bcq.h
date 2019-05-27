cf8ddc0723cf8049ea2b85e900d8cb10745a96b1
Al Gritzmacher <albert@gritzmacher.net>
2019-05-27 10:46:03 -0400

    Working on audio oscillator

--- k3ng_keyer/keyer_pin_settings_k5bcq.h
+++ k3ng_keyer/keyer_pin_settings_k5bcq.h
@@ -21,7 +21,12 @@
 #define tx_key_line_4 0
 #define tx_key_line_5 0
 #define tx_key_line_6 0
-#define sidetone_line 12 //31         // connect a speaker for sidetone   Set to "12" if not using Twin T oscillator
+#ifdef OPTION_SIDETONE_DIGITAL_OUTPUT_NO_SQUARE_WAVE
+#define sidetone_line 31         // connect a speaker for sidetone   Set to "31" for Twin T oscillator
+#endif                          // you still need to move P4A and P4B jumpers
+#ifndef OPTION_SIDETONE_DIGITAL_OUTPUT_NO_SQUARE_WAVE
+#define sidetone_line 12          // connect a speaker for sidetone   Set to "12" if not using Twin T oscillator
+#endif
 #define potentiometer A0        // Speed potentiometer (0 to 5 V) Use pot from 1k to 10k
 #define ptt_tx_1 10              // PTT ("push to talk") lines
 #define ptt_tx_2 11              //   Can be used for keying fox transmitter, T/R switch, or keying slow boatanchors
